086e345ebb1b2aa9283cd575fa9b6a770f638348
Pataballa N V Lakshminarayana <narayana9573944031@gmail.com>
2024-12-27 14:55:40 +0530

    Add project files.

---
+++ UnitTestProject1_SeleniumPracticeMasterProject_4.0/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1_SeleniumPracticeMasterProject_4.0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1_SeleniumPracticeMasterProject_4.0")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2411132-16a9-421e-a9c1-6ffe01fa63f1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1_SeleniumPracticeMasterProject_4.0/UnitTestProject1_SeleniumPracticeMasterProject_4.0.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+
+namespace UnitTestProject1_SeleniumPracticeMasterProject_4._0
+{
+    [TestClass]
+    public class UnitTestProject1_PracticeMasterProjects
+    {
+        public static IWebDriver _driver;
+
+        //public object ScreenshotImageFormate { get; private set; }
+
+        public void SeleniumInit()
+        {
+            _driver = new ChromeDriver();
+            //_driver = chromedriver;
+            //return _driver;
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+        //public void GetScreenshotFileName()
+        //{
+        //    var screenshotPath= @"D:\ExtentReports\screenshot_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
+        //}
+
+       
+        public void CaptrureScreenshots(string _fileName)
+        {
+            try
+            {
+                //var ScreenshotImageFormat;
+                // Cast the driver to ITakesScreenshot
+                ITakesScreenshot screenshotDriver = (ITakesScreenshot)_driver;
+
+                // Capture the screenshot
+                Screenshot screenshot = screenshotDriver.GetScreenshot();
+
+                // Define the path where the screenshot will be saved
+                //string screenshotPath = Path.Combine(Directory.GetCurrentDirectory(), _fileName);
+                string screenshotPath = @"D:\ExtentReports\screenshot_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
+
+                //object ScreenshotImageFormat = null;
+                // Save the screenshot to the specified location
+                screenshot.SaveAsFile(screenshotPath);
+
+                Console.WriteLine("Screenshot saved at: " + screenshotPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error taking screenshot: " + e.Message);
+            }
+        }
+        [TestMethod]
+        public void TestMethod()
+        {
+            try
+            {
+                SeleniumInit();
+                // Open a website for testing
+                _driver.Navigate().GoToUrl("https://www.example.com");
+
+                // Take a screenshot after performing some actions
+                CaptrureScreenshots("test_screenshot.png");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Test failed: " + ex.Message);
+                CaptrureScreenshots("error_screenshot.png");
+            }
+            finally
+            {
+                // Close the browser
+                _driver.Quit();
+            }
+        }
+        [TestMethod]
+        public static void TestExecution()
+        {
+            UnitTestProject1_PracticeMasterProjects test = new UnitTestProject1_PracticeMasterProjects();
+            test.SeleniumInit();
+            test.TestMethod();
+        }
+    }
+}
